4b64313632c26fe3b0e609648a3027c6f8292ada
Justin <jvillacis90@yahoo.com>
2013-04-16 11:48:46 -0700

    added shooting

--- Assets/Scripts/Main Character Scripts/ShootingScript.cs
+++ Assets/Scripts/Main Character Scripts/ShootingScript.cs
@@ -5,6 +5,7 @@ public class ShootingScript : MonoBehaviour {
 	public GameObject startObject;
 	Vector3 startPos;
 	Vector3 endPos;
+	public GameObject projectile;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +13,15 @@ public class ShootingScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+			Vector3 end = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 50));
+			startPos = startObject.transform.position;
+			GameObject proj = Instantiate(projectile, startPos, Quaternion.identity) as GameObject;
+			proj.GetComponent<ProjectileMovementScript>().setDirection(startPos, end);
+			
+		}
 		
 		//Camera.main.ScreenPointToRay(Input.mousePosition)
 	
---
+++ Assets/Scripts/ProjectileMovementScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovementScript : MonoBehaviour {
+	public float speed;
+	Vector3 direction;
+	float deathTimer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deathTimer+= Time.deltaTime;
+		if(deathTimer > 2)
+			Destroy(this.gameObject);
+	}
+	
+	public void setDirection(Vector3 start, Vector3 end)
+	{
+		direction = end - start;
+		direction = Vector3.Normalize(direction);
+		this.rigidbody.AddForce(direction * speed);
+	}
+	
+	
+}
