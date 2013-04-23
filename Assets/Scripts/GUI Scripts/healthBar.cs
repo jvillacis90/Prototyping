39b0f05a81a19722ba7caaf5af06f58edf25a19d
Justin <jvillacis90@yahoo.com>
2013-04-23 09:28:29 -0700

    final BAR HUD

--- Assets/Scripts/GUI Scripts/healthBar.cs
+++ Assets/Scripts/GUI Scripts/healthBar.cs
@@ -5,168 +5,66 @@ using System.Collections.Generic;
 public class healthBar : MonoBehaviour
 {
 	
-PlayerAttributes playerA;
+	PlayerAttributes playerA;
 	
-healthBar healthObj;
-powerBar powerObj;
+	public List<Texture2D> healthTextures = new List<Texture2D>();
+	public List<Texture2D> energyTextures = new List<Texture2D>();	
+	public int currentTexture = 0;
 	
-public int startHealth = 4;
-public int maxHealth = 8;
-public bool destroyOnDeath = false;
-public bool onHitDie = false;
-private float lastHit = 0.0f;
-public bool dead = false;
-	
-public Collider hit;
-	
-public List<Texture> switchableTextures = new List<Texture>();
-public int currentTexture = 0;
-	
-//public Texture healthTex;
-private Texture texture;
-	
-public Texture2D healthTex;
-public Texture2D powerTex;
-	
-
+	public Texture2D healthTex;
+	public Texture2D energyTex;
+		
+	public float barWidth;
+	public float barHeight;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
-		if (switchableTextures.Count > 0) 
-		{
-			texture = switchableTextures[currentTexture];
-		}
-		
+		healthTex = healthTextures[4];
+		energyTex = energyTextures[4];
 		playerA = PlayerAttributes.GetInstance();
-		if(!healthObj)
-		{
-			healthObj = GameObject.FindGameObjectWithTag("Player").GetComponent<healthBar>();
-		}
-		
-		playerA = PlayerAttributes.GetInstance();
-		if(!powerObj)
-		{
-			powerObj = GameObject.FindGameObjectWithTag("Player").GetComponent<powerBar>();
-		}
-
-		
-		maxHealth = startHealth;
 	
 	}
 	
 		
-	public void changeTexture(int switchTo) 
+	public void ChangeHealthTexture(float switchTo) 
 	{
-		if (switchTo < switchableTextures.Count && switchTo >= 0) 
+		int switchInt = (int)switchTo;
+		if (switchTo < healthTextures.Count && switchInt >= 0) 
 		{
-			texture = switchableTextures[switchTo];
-			currentTexture = switchTo;
+			healthTex = healthTextures[switchInt];
 		} 
 
 	}
-	
-		void up() 
+	public void ChangeEnergyTexture(float switchTo) 
 	{
-		if ((currentTexture+1) < switchableTextures.Count) 
+		int switchInt = (int)switchTo;
+		if (switchTo < energyTextures.Count && switchInt >= 0) 
 		{
-			++currentTexture;
-			texture = switchableTextures[currentTexture];
+			energyTex = energyTextures[switchInt];
 		} 
 
 	}
-	
-	void nextTexture() 
-	{
-		if ((currentTexture+1) < switchableTextures.Count) 
-		{ // if we are at the end of the array
-			++currentTexture;
-			texture = switchableTextures[currentTexture];
-		} 
-		else 
-		{// loop to the beginning
-			currentTexture = 0;
-			texture = switchableTextures[currentTexture];
-		}
-	}
-	
-	void down() 
-	{
-		if ((currentTexture-1) >= 0) 
-		{
-			--currentTexture;
-			texture = switchableTextures[currentTexture];
-		} 
 
-	}
-	
-	
-	void StartGame() 
-	{
-		
-		dead = false;
-		startHealth = maxHealth;
-		
-	}
-	
-	public int currentHealth()
-	{
-	
-		return startHealth;
-		
-	}
-
-
-	void PlayerHit() 
-	{
-		/*if(onHitDie) 
-		{
-			Dies();
-		}*/
-		
-		int damage = startHealth-1;
-		
-		startHealth -= damage;
-		startHealth = Mathf.Clamp(startHealth,0,maxHealth);
-		float lastHit = Time.time;
-	}
-	
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetKey("b"))
+		if(Input.GetKeyDown("b"))
 		{
-			//healthBar.changeTexture(healthObj.currentHealth);
+			playerA._health +=1;
+			playerA._energy -=1;
+			ChangeHealthTexture(playerA._health);
+			ChangeEnergyTexture(playerA._energy);
 		}
-
-		/*if (startHealth == 0)
-		{
-			//int startHealth = -1;
-			Dies();
-		}*/
 
 	}
-
-	/*void Dies() 
-	{
-		
-		dead = true;
-	
-		if (destroyOnDeath) 
-		{
-			Destroy(this.gameObject);
-		}
-	}*/
-	
 	void OnGUI()
 	{
 		//Draw the power bar
-		Rect rect = new Rect(0,0,Screen.width/3, Screen.height/10);
-		GUI.Button(rect, healthTex);	
-		rect.y += rect.height;
-		GUI.Button (rect, powerTex);
-		rect.y += rect.height;
+		Rect rect = new Rect(0,0,470/barWidth, 70/barWidth);
+		GUI.Label(rect, healthTex);	
+		rect.y += barHeight;
+		GUI.Label (rect, energyTex);
 	}
 
 }
