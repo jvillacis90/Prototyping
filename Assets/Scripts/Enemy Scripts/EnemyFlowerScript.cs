2aca33a2d09c90551464b91158722f1228d2cbc1
Justin <jvillacis90@yahoo.com>
2013-04-30 17:43:40 -0700

    finish for tentacle enemy behaviors

--- Assets/Scripts/Enemy Scripts/EnemyFlowerScript.cs
+++ Assets/Scripts/Enemy Scripts/EnemyFlowerScript.cs
@@ -4,28 +4,48 @@ using System.Collections;
 public class EnemyFlowerScript : EnemyBaseClass {
 	float distance;
 	GameObject player;
+	int playState = 0;
+	int shootState = 1;
 	float attackTimer = 2;
+	public GameObject projectile;
+	public GameObject shotLocatorObj;
+	public GameObject targetLocatorObj;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		distance = Vector3.Distance(this.transform.position, player.transform.position);
-		if(distance < 75)
+		if(distance < 30)
 		{
-			if(distance < 20)
+			attackTimer+=Time.deltaTime;
+			if(attackTimer < 2 && !animation.isPlaying && playState == 0)
 			{
-				attackTimer+=Time.deltaTime;
-				if(attackTimer < 2 && !animation.IsPlaying("Take 002"))
-					animation.CrossFade("Take 001");
-				if(attackTimer > 2)
-				{
-					animation.CrossFade("Take 002");
-					attackTimer = 0;
-				}
+				animation.CrossFade("Take 001");
+				
+					
 			}
-		}	
+			if(attackTimer > 2)
+			{
+				animation.CrossFade("Take 002");
+				attackTimer = 0;
+				playState = shootState;
+			}
+			if(playState == shootState && !animation.isPlaying)
+			{
+				Debug.Log("here in state");
+				GameObject proj = Instantiate(projectile, shotLocatorObj.transform.position, Quaternion.identity) as GameObject;
+				proj.GetComponent<ProjectileMovementScript>().setDirection(shotLocatorObj.transform.position, targetLocatorObj.transform.position);
+				animation.Play("Take 003");
+				playState = 0;
+			}
+		}
+		else
+		{
+			animation.CrossFade("Take 001");		
+		}
 	}
 }
